f5b309fcc8c2dd024a2a7e713462d0c070daa526
danielrh <danielryanh7@gmail.com>
2022-05-10 21:05:45 -0400

    Change server port

--- server/src/index.js
+++ server/src/index.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(helmet());
